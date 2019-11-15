12c442406125098f2f0412704eb7f127a7af8f34
Calleu Fuzi <calleufuzi@gmail.com>
2019-11-15 01:09:33 -0300

    changed template for ejs

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
